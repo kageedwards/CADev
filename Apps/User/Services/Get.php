120be5e2324368471ad77c1c2d080b58e4949a39
Alex Ramirez <alexir13@gmail.com>
2020-05-08 13:25:59 -0700

    Fix array indices

--- Apps/User/Services/Get.php
+++ Apps/User/Services/Get.php
@@ -8,8 +8,8 @@ class Get {
     public function __construct()
     {
         $this->_testUsers[1] = new User(1, "Hazel", "hazelshork");
-        $this->_testUsers[1] = new User(2, "Comp", "CompThePony");
-        $this->_testUsers[1] = new User(3, "Corona", "corona-ca");
+        $this->_testUsers[2] = new User(2, "Comp", "CompThePony");
+        $this->_testUsers[3] = new User(3, "Corona", "corona-ca");
     }
 
     public function Get($userId = null){
